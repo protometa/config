33f7ff2ce682bab11f11c2c58f6e58ad8910b735
Luke Nimtz <luke.nimtz@gmail.com>
2022-11-15 19:06:48 -0800

    fish vi mode and prompt improvements

--- fish/config.fish
+++ fish/config.fish
@@ -3,12 +3,15 @@ set -U fish_greeting
 # homebrew path
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x EDITOR nvim
+    set -x EDITOR hx
     kubectl completion fish | source
 
     set -x JAVA_HOME "/Users/LNimtz/Library/Caches/Coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz/jdk8u292-b10/Contents/Home"
-
 end
+
+# Set the cursor shapes for the different vi modes.
+set fish_cursor_default     block
+set fish_cursor_insert      line
+set fish_cursor_visual      underscore
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -44,6 +44,14 @@ function fish_prompt --description 'Write out the prompt'
         set suffix '$'
     end
 
+    # non-zero exit code
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    if [ -n "$prompt_status" ]
+      echo -n "$prompt_status "
+    end
+
     # PWD
     set_color $color_cwd
     echo -n (prompt_pwd)
@@ -51,10 +59,6 @@ function fish_prompt --description 'Write out the prompt'
 
     printf '%s ' (fish_vcs_prompt)
 
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color --bold $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-    echo -n $prompt_status
     set_color normal
 
     echo -n "$suffix "
