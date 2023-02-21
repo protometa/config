ac90fd8eae50523c27a199afb75bb3b38e47491b
Luke Nimtz <luke.nimtz@gmail.com>
2023-02-21 15:47:16 -0500

    simpler history fzf

--- fish/functions/_fzf_search_history.fish
+++ fish/functions/_fzf_search_history.fish
@@ -4,13 +4,13 @@ function _fzf_search_history --description "Search command history. Replace the 
 
     set command_with_ts (
         # Reference https://devhints.io/strftime to understand strftime format symbols
-        builtin history --null --show-time="%m-%d %H:%M:%S │ " |
+        builtin history --null --show-time="%m-%d │ " |
         _fzf_wrapper --read0 \
             --tiebreak=index \
             --query=(commandline) \
             # preview current command using fish_ident in a window at the bottom 3 lines tall
             --preview="echo -- {4..} | fish_indent --ansi" \
-            --preview-window="bottom:3:wrap" \
+            --preview-window="bottom:2:wrap" \
             $fzf_history_opts |
         string collect
     )
