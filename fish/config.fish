923f66de6de548298b69595a3fe8ba3248261c37
Luke Nimtz <luke.nimtz@gmail.com>
2023-02-20 21:02:32 -0800

    silence which cross platform

--- fish/config.fish
+++ fish/config.fish
@@ -10,7 +10,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x EDITOR hx
 
-    which -s kubectl
+    which kubectl &> /dev/null
     and kubectl completion fish | source
 
     set -x JAVA_HOME "/Users/LNimtz/Library/Caches/Coursier/arc/https/github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u292-b10/OpenJDK8U-jdk_x64_mac_hotspot_8u292b10.tar.gz/jdk8u292-b10/Contents/Home"
